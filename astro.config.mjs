295e73a8217e38dde11c8b8a186b69e27e4bce51
Reinald Van Praet <reinald.vanpraet@gmail.com>
2025-10-14 07:30:41 +0200

    Debugging GHP deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rvanpraet.github.io',
+  base: '/rvanpraet',
   vite: {
     plugins: [tailwindcss()],
   },
